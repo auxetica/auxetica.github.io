9aee8135be1cf8159f2ce005331444fd0eb4d3db
James Ruan <jamesjuniorruan@gmail.com>
2025-10-26 19:55:29 -0400

    Add new pages and graphics

---
+++ variables1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+print('Content-type: text/html\n')
+
+import cgitb #
+cgitb.enable() #These 2 lines will allow error messages to appear on a web page in the browser
+
+import cgi
+import random
+
